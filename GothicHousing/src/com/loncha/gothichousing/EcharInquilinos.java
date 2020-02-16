a89f603432e5dab0c5beec2bb23912e957fe3546
Lonchadepavo <lonchadevelop@gmail.com>
2020-02-16 17:45:44 +0100

    Nueva opción para echar inquilinos (en desarrollo)

---
+++ GothicHousing/src/com/loncha/gothichousing/EcharInquilinos.java
@@ -0,0 +1,146 @@
+package com.loncha.gothichousing;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.bukkit.Server;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginLoader;
+
+public class EcharInquilinos implements CommandExecutor, Plugin {
+
+	@Override
+	public List<String> onTabComplete(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FileConfiguration getConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public File getDataFolder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ChunkGenerator getDefaultWorldGenerator(String arg0, String arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PluginDescriptionFile getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Logger getLogger() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PluginLoader getPluginLoader() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public InputStream getResource(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Server getServer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isNaggable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onLoad() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reloadConfig() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void saveConfig() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void saveDefaultConfig() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void saveResource(String arg0, boolean arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setNaggable(boolean arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
